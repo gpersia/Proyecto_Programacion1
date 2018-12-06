6876eeb9816926375e3b02076570a70208aefb6c
Gianluca <you@example.com>
2018-12-06 15:04:54 -0300

    listo!

--- exportar_auditoria.php
+++ exportar_auditoria.php
@@ -17,11 +17,21 @@ $datos = $ejecsql -> fetch(PDO::FETCH_ASSOC);
 $exp = "Usuario: ". $datos['username'] . " | Fecha de acceso: " . $datos['fecha_acceso']. " | Tiempo de respuesta: " . $datos['response_time'] . " | Servicio usado: " . $datos['endpoint'];
 $_SESSION['exp'] = $exp;
 
-$archivo = fopen('auditoria.txt', 'w+');
-fwrite($archivo, $_SESSION['exp'].PHP_EOL);
-fclose($archivo);
+// Create temp file
+$tmpName = tempnam(sys_get_temp_dir(), 'data'); // Gets temp directory
+$file = fopen($tmpName, 'w'); // Creates file in temp directory
 
-header("Content-disposition: attachment; filename: auditoria.txt");
-header("Content-type: MIME");
-readfile("auditoria.txt");
+while($row = $ejecsql->fetch(PDO::FETCH_ASSOC)){
+    $str = $row["auditoria_id"] . " " . $row["fecha_acceso"] . " " . $row["user"] . " " . $row["response_time"] . " " . $row["created"] . "," . PHP_EOL;
+    echo $str;
+    fwrite($file, $str);
+}
+
+
+// Headers to download file
+header('Content-Description: File Transfer');
+header('Content-Disposition: attachment; filename=auditoria.txt');
+header('Content-Length: ' . filesize($tmpName));
+
+readfile($tmpName);
 ?>
